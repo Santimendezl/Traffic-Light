dfa19816e049aaf1e172d9078a20bd24dbc6628f
Santimendezl <santimlacosta@gmail.com>
2023-04-19 19:35:39 +0000

    Logical finish

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [ colorRojo, setColorRojo] = useState("bg-danger");
+	const [ colorAmarillo, setColorAmarillo] = useState("bg-warning");
+	const [ colorVerde, setColorVerde] = useState("bg-success");
+
+	function cambiarColorRojo(){
+		setColorRojo("bg-secondary");
+		setColorAmarillo("bg-warning");
+		setColorVerde("bg-success");
+	}
+	function cambiarColorAmarillo(){
+		setColorAmarillo("bg-secondary");
+		setColorRojo ("bg-danger");
+		setColorVerde ("bg-success");
+	}
+	function cambiarColorVerde(){
+		setColorVerde("bg-secondary");
+		setColorAmarillo("bg-warning");
+		setColorRojo ("bg-danger");
+	}
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className=" d-flex flex-column text-center bg-dark" style={{height:'100px',width:'20px'}}>
+			<div className="bg-dark">
+				<button className={"btn rounded-circle "+ colorRojo}  onClick={cambiarColorRojo} style={{height:'100px',width:'100px'}}></button>
+				<button className={"btn rounded-circle "+ colorAmarillo} onClick={cambiarColorAmarillo} style={{height:'100px',width:'100px'}}></button>
+				<button className={"btn rounded-circle "+ colorVerde} onClick={cambiarColorVerde} style={{height:'100px',width:'100px'}}></button>
+			</div> 
+		</div> 
 	);
 };
 
