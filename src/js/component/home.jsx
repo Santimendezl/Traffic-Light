4e8c5fb12b12391f29f070558bf8763592d0d048
Santimendezl <santimlacosta@gmail.com>
2023-04-20 04:44:36 +0000

    Fix Bootstrap

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,34 +3,36 @@ import React, {useState} from "react";
 
 //create your first component
 const Home = () => {
-	const [ colorRojo, setColorRojo] = useState("bg-danger");
-	const [ colorAmarillo, setColorAmarillo] = useState("bg-warning");
-	const [ colorVerde, setColorVerde] = useState("bg-success");
+	const [ colorRojo, setColorRojo] = useState("bg-danger bg-opacity-50");
+	const [ colorAmarillo, setColorAmarillo] = useState("bg-warning bg-opacity-50");
+	const [ colorVerde, setColorVerde] = useState("bg-success bg-opacity-50");
 
 	function cambiarColorRojo(){
-		setColorRojo("bg-secondary");
-		setColorAmarillo("bg-warning");
-		setColorVerde("bg-success");
+		setColorRojo("bg-danger");
+		setColorAmarillo("bg-warning bg-opacity-50");
+		setColorVerde("bg-success bg-opacity-50");
 	}
 	function cambiarColorAmarillo(){
-		setColorAmarillo("bg-secondary");
-		setColorRojo ("bg-danger");
-		setColorVerde ("bg-success");
+		setColorAmarillo("bg-warning");
+		setColorRojo ("bg-danger bg-opacity-50");
+		setColorVerde ("bg-success bg-opacity-50");
 	}
 	function cambiarColorVerde(){
-		setColorVerde("bg-secondary");
-		setColorAmarillo("bg-warning");
-		setColorRojo ("bg-danger");
+		setColorVerde("bg-success");
+		setColorAmarillo("bg-warning bg-opacity-50");
+		setColorRojo ("bg-danger bg-opacity-50");
 	}
 
 	return (
-		<div className=" d-flex flex-column text-center bg-dark" style={{height:'100px',width:'20px'}}>
-			<div className="bg-dark">
-				<button className={"btn rounded-circle "+ colorRojo}  onClick={cambiarColorRojo} style={{height:'100px',width:'100px'}}></button>
-				<button className={"btn rounded-circle "+ colorAmarillo} onClick={cambiarColorAmarillo} style={{height:'100px',width:'100px'}}></button>
-				<button className={"btn rounded-circle "+ colorVerde} onClick={cambiarColorVerde} style={{height:'100px',width:'100px'}}></button>
-			</div> 
+		<>
+		<div className="container text-center bg-dark" style={{height:100,width:20}}></div> 
+		<div className="container text-center d-flex flex-column bg-dark align-items-center rounded justify-content-around" style={{height:400,width:150}}>
+			<div className={"rounded-circle"}><button type="button" className={"btn rounded-circle "+ colorRojo}  onClick={cambiarColorRojo} style={{height:'100px',width:'100px'}}></button></div>
+			<div><button type="button" className={"btn rounded-circle "+ colorAmarillo} onClick={cambiarColorAmarillo} style={{height:'100px',width:'100px'}}></button></div>
+			<div><button type="button" className={"btn rounded-circle "+ colorVerde} onClick={cambiarColorVerde} style={{height:'100px',width:'100px'}}></button></div>
 		</div> 
+		
+		</>
 	);
 };
 
