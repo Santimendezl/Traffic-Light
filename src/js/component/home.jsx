7daa6d96084d64da0b7ebab2d558062c1f9e141f
Santimendezl <santimlacosta@gmail.com>
2023-04-21 09:09:23 +0000

    Remove some divs

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,7 +3,7 @@ import React, {useState} from "react";
 
 //create your first component
 const Home = () => {
-	const [ colorRojo, setColorRojo] = useState("bg-danger bg-opacity-50");
+	const [ colorRojo, setColorRojo] = useState("bg-danger bg-opacity-50"); 
 	const [ colorAmarillo, setColorAmarillo] = useState("bg-warning bg-opacity-50");
 	const [ colorVerde, setColorVerde] = useState("bg-success bg-opacity-50");
 
@@ -27,9 +27,9 @@ const Home = () => {
 		<>
 		<div className="container text-center bg-dark" style={{height:100,width:20}}></div> 
 		<div className="container text-center d-flex flex-column bg-dark align-items-center rounded justify-content-around" style={{height:400,width:150}}>
-			<div className={"rounded-circle"}><button type="button" className={"btn rounded-circle "+ colorRojo}  onClick={cambiarColorRojo} style={{height:'100px',width:'100px'}}></button></div>
-			<div><button type="button" className={"btn rounded-circle "+ colorAmarillo} onClick={cambiarColorAmarillo} style={{height:'100px',width:'100px'}}></button></div>
-			<div><button type="button" className={"btn rounded-circle "+ colorVerde} onClick={cambiarColorVerde} style={{height:'100px',width:'100px'}}></button></div>
+			<button type="button" className={"btn rounded-circle "+ colorRojo}  onClick={cambiarColorRojo} style={{height:'100px',width:'100px'}}></button>
+			<button type="button" className={"btn rounded-circle "+ colorAmarillo} onClick={cambiarColorAmarillo} style={{height:'100px',width:'100px'}}></button>
+			<button type="button" className={"btn rounded-circle "+ colorVerde} onClick={cambiarColorVerde} style={{height:'100px',width:'100px'}}></button>
 		</div> 
 		
 		</>
